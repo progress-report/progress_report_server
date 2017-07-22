189315cbd167d57a043e020c4c3247b5dd2bf2e7
Lee Sharma <lee@leesharma.com>
2017-07-22 15:54:47 -0400

    Fake the data!
    Marshal dump and load generated state data so that we have something at
    least

--- app/models/state_election_data.rb
+++ app/models/state_election_data.rb
@@ -6,15 +6,29 @@ require 'efficiency_gap'
 # columns, this will provide a single interface for the API.
 class StateElectionData
   def initialize
-    @state = StateVoterData.new(
-      [
-        DistrictVoterData.new({democrat:75, republican:25}, 100),
-        DistrictVoterData.new({democrat:60, republican:40}, 100),
-        DistrictVoterData.new({democrat:43, republican:57}, 100),
-        DistrictVoterData.new({democrat:48, republican:52}, 100),
-        DistrictVoterData.new({democrat:49, republican:51}, 100),
-      ]
-    )
+    @state = Marshal.load "\x04\bo:\x13StateVoterData\x06:\x0F@districts[\x15S:\x16DistrictVoterData\a:\n" +
+      "votes{\a:\x0Frepublicani\x02T\x05:\rdemocrati\x02\xE8\x03:\n" +
+      "totali\x02\xFB\tS;\a\a;\b{\a;\ti\x02\v\a;\n" +
+      "i\x02\xE8\x03;\vi\x02N\vS;\a\a;\b{\a;\n" +
+      "i\x02w\x06;\ti\x02\xE8\x03;\vi\x02\xC8\n" +
+      "S;\a\a;\b{\a;\ti\x02\x8D\x04;\n" +
+      "i\x02\xE8\x03;\vi\x02\xA4\bS;\a\a;\b{\a;\ti\x02\xD9\x04;\n" +
+      "i\x02\xE8\x03;\vi\x02\xE8\bS;\a\a;\b{\a;\ti\x02D\x05;\n" +
+      "i\x02\xE8\x03;\vi\x02\xC5\tS;\a\a;\b{\a;\ti\x02$\a;\n" +
+      "i\x02\xE8\x03;\vi\x02\x82\vS;\a\a;\b{\a;\ti\x02\xD8\x04;\n" +
+      "i\x02\xE8\x03;\vi\x02\x06\tS;\a\a;\b{\a;\n" +
+      "i\x02.\a;\ti\x02\xE8\x03;\vi\x02\x97\vS;\a\a;\b{\a;\ti\x02\xC5\x05;\n" +
+      "i\x02\xE8\x03;\vi\x02V\n" +
+      "S;\a\a;\b{\a;\n" +
+      "i\x02\x0E\x06;\ti\x02\xE8\x03;\vi\x02\x04\n" +
+      "S;\a\a;\b{\a;\ti\x026\x06;\n" +
+      "i\x02\xE8\x03;\vi\x02t\n" +
+      "S;\a\a;\b{\a;\n" +
+      "i\x02s\x05;\ti\x02\xE8\x03;\vi\x02!\n" +
+      "S;\a\a;\b{\a;\ti\x02j\x04;\n" +
+      "i\x02\xE8\x03;\vi\x02^\bS;\a\a;\b{\a;\ti\x02)\x05;\n" +
+      "i\x02\xE8\x03;\vi\x02\xA4\tS;\a\a;\b{\a;\ti\x02/\x05;\n" +
+      "i\x02\xE8\x03;\vi\x02\xD9\t"
   end
 
   def districts
