6fbfa73424d1c84d07d0b967e9df78d61e8b8365
Lee Sharma <lee@leesharma.com>
2017-08-10 15:41:47 -0400

    Enable CORS for all API endpoints
    Fixes #14
    
    This commit adds the Flask-CORS dependency to allow all cross-site
    requests on our API. There is no CORS testcase yet, but I wrote a stub
    and a note.
    
    Here's a link to Flask-CORS:
    
      https://flask-cors.readthedocs.io/en/latest/
    
    TODO:
    -----
    I didn't add a test case for cross-origin requests. I only had about 30
    mins right now, and I couldn't get a valid test in that time. Since the
    front-end development relies on this feature, I think it's worth
    shipping now.
    
    Alternate implementation:
    -------------------------
    Using Flask-CORS was quick and easy for now, but if we want to remove
    that dependency in the future, here's a code snippet showing how to add
    a `crossdomain` decorator:
    
      http://flask.pocoo.org/snippets/56/

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,8 +1,7 @@
-import pytest
+# -*- coding: utf-8 -*-
+"""Integration tests for the Scorecard API"""
 
-#
-# Integration tests for API endpoints
-#
+import pytest
 
 @pytest.fixture
 def app():
@@ -13,6 +12,7 @@ def app():
     return progress_report.app.test_client()
 
 def init_db(db):
+    """Helper method to seed the database"""
     # Temporary solution until we have proper database migrations and can
     # setup/teardown the database between tests
     records = [
@@ -36,21 +36,31 @@ def init_db(db):
 
 
 def test_api_root_endpoint(app):
+    """Make sure the api root (/api/) works"""
     assert 'application/json' == app.get('/api/').content_type
 
 def test_root_endpoint_without_trailing_slash(app):
+    """Make sure /api redirects to /api/"""
     assert '301 MOVED PERMANENTLY' == app.get('/api').status
     # assert 'application/json' == app.get('/api').content_type
 
 def test_states_endpoint(app):
+    """Make sure the states resource (/api/states/) works"""
     assert 'application/json' == app.get('/api/states/').content_type
 
 def test_states_endpoint_without_trailing_slash(app):
+    """Make sure /api/states redirects to /api/states/"""
     assert '301 MOVED PERMANENTLY' == app.get('/api/states').status
     # assert 'application/json' == app.get('/api/states').content_type
 
 def test_ohio_endpoint(app):
+    """Make sure the Ohio state endpoint (/api/states/ohio) works"""
     assert 'application/json' == app.get('/api/states/ohio').content_type
+
+def test_cors(app):
+    """Make sure cross-origin requests are permitted by api endpoints"""
+    # TODO: figure out how to implement this test
+    pass
 
 #
 # We expect it to be in the following form:
@@ -70,15 +80,20 @@ def test_ohio_endpoint(app):
 #   }
 #
 def test_ohio_endpoint_fields(app):
+    """Make sure the Ohio state endpoint returns the right data"""
+
     import json
     response_body = json.loads(app.get('/api/states/ohio').data)
 
+    # verify state-level attributes
     assert 'districts' in response_body['data']
     assert 'efficiency_gap' in response_body['data']
     assert 'wasted_votes' in response_body['data']
 
+    # verify state number
     assert 5 == len(response_body['data']['districts'])
 
+    # verify district-level attributes
     district_data = response_body['data']['districts']['1']
     assert 'd_votes' in district_data
     assert 'r_votes' in district_data
